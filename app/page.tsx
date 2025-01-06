4023730b2dd7663b288a34cad2d2878cb7377b67
mctuliomontoya <mctulio.2@gmail.com>
2025-01-06 15:21:35 -0700

    Add landing to index page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 "use client"
 import {MarcoPhoto } from '@/app/components/MarcoPhoto'
+import Landing from '@/app/sections/Landing'
 
 export default function Home() {
   return (
     <>
+      <Landing />
       <div id='background'>
       </div>
       <div className='font-mono text-[8px] leading-3' dangerouslySetInnerHTML={{ __html: MarcoPhoto }}></div>

98fc48b075247c3d36897a76207af776c437ec0c
mctuliomontoya <mctulio.2@gmail.com>
2025-01-09 19:37:20 -0700

    delete non used import

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import { FooterSection } from '@/app/sections/Footer'
-import Landing from '@/app/sections/Landing'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',

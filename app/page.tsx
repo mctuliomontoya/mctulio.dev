6598c41aa877679cd65b8a2743abc0697b8d939a
mctuliomontoya <mctulio.2@gmail.com>
2025-01-14 20:18:28 -0700

    feat: add about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
-import {MarcoPhoto } from '@/app/components/MarcoPhoto'
 import Landing from '@/app/sections/Landing'
 import Hero from '@/app/sections/Hero'
+import About from '@/app/sections/About'
 
 export default function Home() {
   return (
@@ -10,10 +10,7 @@ export default function Home() {
       <div id='background'>
       </div>
       <Hero />
-      <div className='font-mono text-[6px] md:text-[8px] md:leading-[0.7rem] leading-[0.5rem]' dangerouslySetInnerHTML={{ __html: MarcoPhoto }}></div>
-      <div className='font-mono text-[8px] leading-3' dangerouslySetInnerHTML={{ __html: MarcoPhoto }}></div>
-      <div className='font-mono text-[8px] leading-3' dangerouslySetInnerHTML={{ __html: MarcoPhoto }}></div>
-      <div className='font-mono text-[8px] leading-3' dangerouslySetInnerHTML={{ __html: MarcoPhoto }}></div>
+      <About />
     </>
 
   );

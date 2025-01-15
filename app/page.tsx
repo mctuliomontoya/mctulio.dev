c8cfb689f9b9de38fc8cf5a13af0034e288ab605
mctuliomontoya <mctulio.2@gmail.com>
2025-01-15 16:59:36 -0700

    remove scrollbar

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,14 @@
 import Landing from '@/app/sections/Landing'
 import Hero from '@/app/sections/Hero'
 import About from '@/app/sections/About'
+import './globals.css'
 
 export default function Home() {
   return (
     <>
-      <Landing />
       <div id='background'>
       </div>
+      <Landing />
       <Hero />
       <About />
     </>

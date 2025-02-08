c9c5faaea1968a8f9351ae8b5f783fd61bac1dda
mctuliomontoya <mctulio.2@gmail.com>
2025-02-07 19:58:28 -0700

    change to display current projects

--- app/components/PortfolioGrid.tsx
+++ app/components/PortfolioGrid.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { JSX, useState } from 'react'
 import Image from 'next/image'
 import { motion, AnimatePresence } from 'framer-motion'
 import ProjectModal from './ProjectModal'
@@ -9,42 +9,93 @@ import { twMerge } from 'tailwind-merge'
 interface Project {
   id: number
   title: string
-  description: string
+  date: string
+  description: JSX.Element
   images: string[]
+  link?: string
 }
 
 const projects: Project[] = [
   {
     id: 1,
-    title: "Project 1",
-    description: "This is a detailed description of Project 1. It was a challenging project that involved creating a responsive web application using React and Next.js. The main features include user authentication, real-time data updates, and integration with a third-party API. We faced several challenges during development, particularly in optimizing performance for mobile devices, but overcame them through careful code refactoring and the use of efficient data structures.",
+    title: "AI Sorting Process Optimization",
+    date: "Oct 2023 - Feb 2024",
+    description: <ul className='pl-5 space-y-3 list-disc marker:text-foreground text-white'>
+      <li>Created several Machine Vision models to classify and filter defective parts in assembly plants.</li>
+      <li>Using Python for data augmentation and segmentation to provide models with quality data.</li>
+      <li> Set up and calibration of hardware tools such as cameras, lenses and lights for data acquisition, floor assembly and live
+        data classification.</li>
+      <li>The project was carried out as part of a hackathon, providing a valuable solution that will be used in the industry.</li>
+    </ul>,
     images: [
-      "/im1.jpeg?height=300&width=300",
-      "/im3.png?height=300&width=300",
-      "/im4.png?height=300&width=300",
+      "/images/projects/te-ai-cup/01.webp?height=300&width=300",
+      "/images/projects/te-ai-cup/02.webp?height=300&width=300",
+      "/images/projects/te-ai-cup/03.webp?height=300&width=300",
+      "/images/projects/te-ai-cup/04.webp?height=300&width=300",
+      "/images/projects/te-ai-cup/05.webp?height=300&width=300",
+      "/images/projects/te-ai-cup/06.webp?height=300&width=300",
+      "/images/projects/te-ai-cup/07.webp?height=300&width=300",
+      "/images/projects/te-ai-cup/08.webp?height=300&width=300",
+      "/images/projects/te-ai-cup/09.gif?height=300&width=300",
     ],
   },
   {
     id: 2,
-    title: "Project 2",
-    description: "Project 2 was an e-commerce platform built with a focus on user experience and performance. We implemented a custom shopping cart solution, integrated various payment gateways, and created a robust product management system for administrators. One of the key challenges was ensuring fast page load times despite having a large product catalog. We solved this by implementing efficient caching strategies and lazy loading techniques.",
-    images: ["/im6.png?height=300&width=300", "/im4.png?height=300&width=300"],
+    title: "BlockForge - Blockchain Mockup Page",
+    date: "Nov 2024",
+    description:
+    <ul className='pl-5 space-y-3 list-disc marker:text-foreground text-white'>
+      <li>Collaborated with a team to digitalize a company’s process into a React Native application.</li>
+      <li>The app allows users to create and review logs depending on their role, and raise incident reports.</li>
+      <li>Sent notifications when room temperatures were detected out of range.</li>
+      <li>Used TailwindCSS to give the application a modern, compact and intuitive design.</li>
+    </ul>,
+    images: [
+      "/images/projects/blockforge/00.webp?height=300&width=300",
+      "/images/projects/blockforge/01.webp?height=300&width=300",
+      "/images/projects/blockforge/02.webp?height=300&width=300",
+      "/images/projects/blockforge/03.webp?height=300&width=300",
+      "/images/projects/blockforge/04.webp?height=300&width=300",
+      "/images/projects/blockforge/05.webp?height=300&width=300",
+      "/images/projects/blockforge/06.webp?height=300&width=300",
+    ],
+    link: "https://frontend-saas.vercel.app/"
   },
   {
     id: 3,
-    title: "Project 3",
-    description: "Project 3 focused on developing a mobile app for a startup in the fitness industry. We used React Native to create a cross-platform solution that works seamlessly on both iOS and Android. The app features personalized workout plans, progress tracking, and social sharing capabilities. One of our main challenges was optimizing the app's performance and ensuring smooth animations across different devices.",
+    title: "Company incident and logs manager",
+    date: "Sep - Dec 2023",
+    description:
+      <ul className='pl-5 space-y-3 list-disc marker:text-foreground text-white'>
+        <li>Collaborated with a team to digitalize a company’s process into a React Native application.</li>
+        <li>The app allows users to create and review logs depending on their role, and raise incident reports.</li>
+        <li>Sent notifications when room temperatures were detected out of range.</li>
+        <li>Used TailwindCSS to give the application a modern, compact and intuitive design.</li>
+      </ul>,
     images: [
-      "/im5.png?height=300&width=300",
-      "/im2.jpeg?height=300&width=300",
-      "/im6.png?height=300&width=300",
+      "/images/projects/bamx/bamx.svg?height=300&width=300",
+      "/images/projects/bamx/01.webp?height=300&width=300",
+      "/images/projects/bamx/02.webp?height=300&width=300",
+      "/images/projects/bamx/03.webp?height=300&width=300",
+      "/images/projects/bamx/04.webp?height=300&width=300",
     ],
   },
   {
     id: 4,
-    title: "Project 4",
-    description: "For Project 4, we developed a data visualization dashboard for a large corporation. Using D3.js and React, we created interactive charts and graphs that help the company make data-driven decisions. The main challenge was handling and efficiently rendering large datasets without compromising the user experience. We implemented various optimization techniques, including data aggregation and lazy loading of chart components.",
-    images: ["/im8.png?height=300&width=300", "/im7.jpeg?height=300&width=300"],
+    title: "Coronavirus variations database",
+    date: "Mar - Apr 2022",
+    description:
+      <ul className='pl-5 space-y-3 list-disc marker:text-foreground text-white'>
+        <li>Collaborated with a team to develop a Rstudio project that reads, orients, aligns and displays tables to compare different genomes deviated from SARS-CoV-2. </li>
+        <li>Registration of data from different types of genome derived from Coronavirus.</li>
+        <li>Reading and manipulation of 29,000+ character genome sequences.</li>
+        <li>Interpretation of data through graphical resources created by an R script.</li>
+      </ul>,
+    images: [
+      "/images/projects/covid-alignments/01.webp?height=300&width=300",
+      "/images/projects/covid-alignments/02.webp?height=300&width=300",
+      "/images/projects/covid-alignments/03.webp?height=300&width=300",
+    ],
   },
 ]
 
@@ -75,7 +126,7 @@ export default function PortfolioGrid() {
             onClick={() => setSelectedId(project.id)}
             className={twMerge(
               "cursor-pointer overflow-hidden col-span-2 rounded-lg shadow-lg",
-              project.id === 1 || project.id === 4 ? "md:col-span-1" : "md:col-span-3"
+              project.id === 1 || project.id === 4 ? "md:col-span-3" : "md:col-span-1"
             )}
             variants={{
               hidden: { opacity: 0, y: 20 },
